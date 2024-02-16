2224c2d7076f3d8b63c2e3d94a51471a0ce71547
Joseph Delaney <jdelaney7340@gmail.com>
2024-02-15 21:49:00 -0500

    first commit

---
+++ build/apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/josep/Desktop/pybind_test/apps
+# Build directory: C:/Users/josep/Desktop/pybind_test/build/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
